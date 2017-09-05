28219227ee9b8069d5da9e9b1359f1d2a4177c8c
XUFENG-PC\xufeng <1277032935@qq.com>
2017-09-05 21:26:05 +0800

    修改redistool服务

--- src/main/java/com/xwintop/xJavaFxTool/controller/debugTools/redisTool/RedisToolController.java
+++ src/main/java/com/xwintop/xJavaFxTool/controller/debugTools/redisTool/RedisToolController.java
@@ -1,13 +1,34 @@
 package com.xwintop.xJavaFxTool.controller.debugTools.redisTool;
 
-import com.xwintop.xJavaFxTool.view.debugTools.redisTool.RedisToolView;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.xwintop.xJavaFxTool.services.debugTools.redisTool.RedisToolService;
+import com.xwintop.xJavaFxTool.view.debugTools.redisTool.RedisToolView;
+import com.xwintop.xcore.util.javafx.TooltipUtil;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import lombok.Getter;
 
+@Getter
 public class RedisToolController extends RedisToolView {
+	private RedisToolService redisToolService = new RedisToolService(this);
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		initView();
@@ -21,6 +42,42 @@ public class RedisToolController extends RedisToolView {
 	}
 
 	private void initEvent() {
+		redisServiceTreeView.setOnMouseClicked(event -> {
+			if (event.getButton() == MouseButton.SECONDARY) {
+				MenuItem menu_addServer = new MenuItem("添加服务器");
+				menu_addServer.setOnAction(event1 -> {
+					int row = 0;
+
+					GridPane page1Grid = new GridPane();
+					page1Grid.setVgap(10);
+					page1Grid.setHgap(10);
+
+					TextField txName = createTextField(null, page1Grid, "Name:", row++);
+					TextField txHost = createTextField("localhost", page1Grid, "Host:", row++);
+					TextField txPort = createTextField("6379", page1Grid, "Port:", row++);
+					TextField txPassword = createTextField(null, page1Grid, "Password:", row++);
+
+					Alert alert = new Alert(Alert.AlertType.NONE, null, new ButtonType("取消", ButtonBar.ButtonData.NO),
+							new ButtonType("确定", ButtonBar.ButtonData.YES));
+					alert.setTitle("添加服务器");
+					alert.setGraphic(page1Grid);
+					alert.setWidth(200);
+					Optional<ButtonType> _buttonType = alert.showAndWait();
+					// 根据点击结果返回
+					if (_buttonType.get().getButtonData().equals(ButtonBar.ButtonData.YES)) {
+						if (StringUtils.isEmpty(txName.getText()) || StringUtils.isEmpty(txHost.getText())
+								|| StringUtils.isEmpty(txPort.getText())) {
+							TooltipUtil.showToast("请输入服务器信息");
+							return;
+						}
+					}
+				});
+				MenuItem menu_Refresh = new MenuItem("刷新");
+				menu_Refresh.setOnAction(event1 -> {
+				});
+				redisServiceTreeView.setContextMenu(new ContextMenu(menu_addServer, menu_Refresh));
+			}
+		});
 	}
 
 	private void initService() {
@@ -40,5 +97,13 @@ public class RedisToolController extends RedisToolView {
 
 	@FXML
 	private void treeRefurbishAction(ActionEvent event) {
+	}
+
+	private TextField createTextField(String textValue, GridPane gridPane, String label, int row) {
+		TextField textField = new TextField(textValue);
+		GridPane.setHgrow(textField, Priority.ALWAYS);
+		gridPane.add(new Label(label), 0, row);
+		gridPane.add(textField, 1, row);
+		return textField;
 	}
 }
--- src/main/java/com/xwintop/xJavaFxTool/services/debugTools/redisTool/RedisToolService.java
+++ src/main/java/com/xwintop/xJavaFxTool/services/debugTools/redisTool/RedisToolService.java
@@ -1,23 +1,36 @@
 package com.xwintop.xJavaFxTool.services.debugTools.redisTool;
 
+import java.util.HashMap;
 import java.util.Map;
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.Message;
+import org.apache.commons.lang3.StringUtils;
 
-import org.quartz.SchedulerFactory;
-
-import com.xwintop.xJavaFxTool.controller.debugTools.ActiveMqToolController;
-import com.xwintop.xJavaFxTool.services.debugTools.ActiveMqToolService;
+import com.xwintop.xJavaFxTool.controller.debugTools.redisTool.RedisToolController;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
+import redis.clients.jedis.Jedis;
 
 @Getter
 @Setter
 @Log4j
 public class RedisToolService {
+	private RedisToolController redisToolController;
+	private Map<String,Jedis> jedisMap = new HashMap<String, Jedis>();
+
+	public RedisToolService(RedisToolController redisToolController) {
+		this.redisToolController = redisToolController;
+	}
+	
+	public void addServiceAddress(String name,String host,int port,String password){
+//		Jedis jedis = new Jedis("localhost", 6379);
+		Jedis jedis = new Jedis(host, port);
+		if(StringUtils.isNotEmpty(password)){
+			jedis.auth(password);
+		}
+		jedis.getDB();
+		jedisMap.put(name, jedis);
+	}
 
 }
