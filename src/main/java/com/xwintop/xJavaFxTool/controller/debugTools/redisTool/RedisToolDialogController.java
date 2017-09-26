481db9d7a15be9cf6b7a94fb86884c5966f8ad4a
1277032935@qq.com <1277032935@qq.com>
2017-09-27 00:25:55 +0800

    添加RedisTool工具类添加值方法。

--- src/main/java/com/xwintop/xJavaFxTool/controller/debugTools/redisTool/RedisToolDialogController.java
+++ src/main/java/com/xwintop/xJavaFxTool/controller/debugTools/redisTool/RedisToolDialogController.java
@@ -1,9 +1,11 @@
 package com.xwintop.xJavaFxTool.controller.debugTools.redisTool;
 
+import com.xwintop.xJavaFxTool.services.debugTools.redisTool.RedisToolDataTableService;
 import com.xwintop.xJavaFxTool.utils.JavaFxViewUtil;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
 
@@ -14,11 +16,17 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -29,7 +37,9 @@ public class RedisToolDialogController implements Initializable
     @FXML
     private TextField keyTextField;
     @FXML
-    private TableView dialogTableView;
+    private TextField overdueTimeTextField;
+    @FXML
+    private TableView<Map<String,String>> dialogTableView;
     @FXML
     private TableColumn dialogKeyTableColumn;
     @FXML
@@ -40,64 +50,113 @@ public class RedisToolDialogController implements Initializable
     private Button cancelButton;
 
     private ObservableList<Map<String, String>> dialogTableData = FXCollections.observableArrayList();
-    private boolean isEnter = false;
+    private RedisToolDataTableService redisToolDataTableService;
+    private String title;
 
-    private Alert alert = null;
+    private Stage dialogStage;
 
-    public static FXMLLoader getFXMLLoader() {
+
+    public static FXMLLoader getFXMLLoader()
+    {
         URL url = Object.class.getResource("/fxml/debugTools/redisTool/RedisToolDialog.fxml");
         FXMLLoader fXMLLoader = new FXMLLoader(url);
         return fXMLLoader;
     }
 
-    public static RedisToolDialogController getRedisToolDialogController(String title,boolean type){
+    public static RedisToolDialogController getRedisToolDialogController(String title, boolean type)
+    {
         FXMLLoader fXMLLoader = RedisToolDialogController.getFXMLLoader();
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle(title);
+        Parent loginNode;
+        Stage dialogStage = null;
         try
         {
-            alert.setGraphic(fXMLLoader.load());
+            loginNode = fXMLLoader.load();
+            dialogStage = new Stage();
+            dialogStage.setTitle("添加"+title+"数据");
+            dialogStage.initModality(Modality.APPLICATION_MODAL);
+//            dialogStage.initOwner(getPrimaryStage());
+            dialogStage.setScene(new Scene(loginNode));
+            dialogStage.setMaximized(false);
+            dialogStage.setResizable(false);
+            dialogStage.show();
         } catch (IOException e)
         {
             e.printStackTrace();
         }
         RedisToolDialogController redisToolDialogController = fXMLLoader.getController();
         redisToolDialogController.setDialogType(type);
-        redisToolDialogController.setAlert(alert);
+        redisToolDialogController.setDialogStage(dialogStage);
+        redisToolDialogController.setTitle(title);
         return redisToolDialogController;
     }
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL location, ResourceBundle resources)
+    {
         initView();
         initEvent();
         initService();
     }
-    private void initView() {
+
+    private void initView()
+    {
         JavaFxViewUtil.setTableColumnMapValueFactory(dialogKeyTableColumn, "key");
         JavaFxViewUtil.setTableColumnMapValueFactory(dialogValueTableColumn, "value");
         dialogTableView.setItems(dialogTableData);
     }
-    private void initEvent() {}
-    private void initService() {}
 
-    public void setDialogType(boolean type){
-        if(type){
-            dialogTableView.getColumns().remove(dialogKeyTableColumn);
-        }
-    }
-    @FXML
-    private void enterAction(ActionEvent event){
-        isEnter = true;
-        Platform.runLater(()->{
-            alert.close();
+    private void initEvent()
+    {
+        dialogTableView.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.SECONDARY) {
+                MenuItem menuAdd = new MenuItem("添加行");
+                menuAdd.setOnAction(event1 -> {
+                    Map<String,String> map =  new HashMap<String,String>();
+                    dialogTableData.add(map);
+                });
+                MenuItem menu_Copy = new MenuItem("复制选中行");
+                menu_Copy.setOnAction(event1 -> {
+                    Map<String,String> map = dialogTableView.getSelectionModel().getSelectedItem();
+                    Map<String,String> map2 =  new HashMap<String,String>(map);
+                    dialogTableData.add(dialogTableView.getSelectionModel().getSelectedIndex(), map2);
+                });
+                MenuItem menu_Remove = new MenuItem("删除选中行");
+                menu_Remove.setOnAction(event1 -> {
+                    dialogTableData.remove(dialogTableView.getSelectionModel().getSelectedItem());
+                });
+                MenuItem menu_RemoveAll = new MenuItem("删除所有");
+                menu_RemoveAll.setOnAction(event1 -> {
+                    dialogTableData.clear();
+                });
+                dialogTableView.setContextMenu(new ContextMenu(menuAdd,menu_Copy, menu_Remove, menu_RemoveAll));
+            }
         });
     }
 
+    private void initService()
+    {
+    }
+
+    public void setDialogType(boolean type)
+    {
+        if (type)
+        {
+            dialogTableView.getColumns().remove(dialogKeyTableColumn);
+        }
+    }
+
     @FXML
-    private void cancelAction(ActionEvent event){
-        Platform.runLater(()->{
-            alert.close();
+    private void enterAction(ActionEvent event)
+    {
+        redisToolDataTableService.addTableDataByDialog();
+        dialogStage.close();
+    }
+
+    @FXML
+    private void cancelAction(ActionEvent event)
+    {
+        Platform.runLater(() -> {
+            dialogStage.close();
         });
     }
 }
--- src/main/java/com/xwintop/xJavaFxTool/services/debugTools/redisTool/RedisToolDataTableService.java
+++ src/main/java/com/xwintop/xJavaFxTool/services/debugTools/redisTool/RedisToolDataTableService.java
@@ -10,15 +10,16 @@ import com.xwintop.xcore.util.javafx.AlertUtil;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
@@ -31,151 +32,183 @@ import lombok.extern.log4j.Log4j;
 @Setter
 @Log4j
 public class RedisToolDataTableService {
-	private RedisToolDataTableController redisToolDataTableController;
-	private Map<String, Tab> dataViewTabMap = new HashMap<String, Tab>();
 
-	public RedisToolDataTableService(RedisToolDataTableController redisToolDataTableController) {
-		this.redisToolDataTableController = redisToolDataTableController;
-	}
+    private RedisToolDataTableController redisToolDataTableController;
+    private Map<String, Tab> dataViewTabMap = new HashMap<String, Tab>();
 
-	public void addTableViewContextMenu() {
-		Menu menuAdd = new Menu("添加");
-		MenuItem menuAddString = new MenuItem("String");
-		menuAddString.setOnAction(event1 -> {
-			addString();
-			reloadTableData();
-		});
-		MenuItem menuAddList = new MenuItem("List");
-		menuAddList.setOnAction(event1 -> {
-			addList();
-			reloadTableData();
-		});
-		MenuItem menuAddSet = new MenuItem("Set");
-		menuAddSet.setOnAction(event1 -> {
-			addSet();
-			reloadTableData();
-		});
-		MenuItem menuAddSortedSet = new MenuItem("Sorted Set");
-		menuAddSortedSet.setOnAction(event1 -> {
-			addSortedSet();
-			reloadTableData();
-		});
-		MenuItem menuAddHash = new MenuItem("Hash");
-		menuAddHash.setOnAction(event1 -> {
-			addHash();
-			reloadTableData();
-		});
-		menuAdd.getItems().addAll(menuAddString, menuAddList, menuAddSet, menuAddSortedSet, menuAddHash);
-		MenuItem menuReload = new MenuItem("刷新");
-		menuReload.setOnAction(event1 -> {
-			reloadTableData();
-		});
-		MenuItem menu_Remove = new MenuItem("删除选中行");
-		menu_Remove.setOnAction(event1 -> {
-			RedisToolDataTableBean redisToolDataTableBean = redisToolDataTableController.getDataTableView()
-					.getSelectionModel().getSelectedItem();
-			if (redisToolDataTableBean != null) {
-				redisToolDataTableController.getRedisUtil().del(redisToolDataTableBean.getName());
-				redisToolDataTableController.getTableData().remove(redisToolDataTableBean);
-				Tab tab = dataViewTabMap.get(redisToolDataTableBean.getName());
-				if(tab != null){
-					redisToolDataTableController.getRedisToolController().getDataViewTabPane().getTabs().remove(tab);
-					dataViewTabMap.remove(redisToolDataTableBean.getName());
-				}
-			}
-		});
-		MenuItem menu_RemoveAll = new MenuItem("删除所有");
-		menu_RemoveAll.setOnAction(event1 -> {
-			redisToolDataTableController.getRedisUtil().delAll();
-			dataViewTabMap.forEach((String k, Tab tab)->{
-				redisToolDataTableController.getRedisToolController().getDataViewTabPane().getTabs().remove(tab);
-			});
-			dataViewTabMap.clear();
-			redisToolDataTableController.getTableData().clear();
-		});
-		redisToolDataTableController.getDataTableView()
-				.setContextMenu(new ContextMenu(menuAdd, menuReload, menu_Remove, menu_RemoveAll));
-	}
+    private RedisToolDialogController redisToolDialogController;
 
-	private void addString() {
-		String[] valueStrings = AlertUtil.showInputAlert("请输入值", "key", "value", "过期时间（留空为永久）");
-		if (valueStrings != null) {
-			redisToolDataTableController.getRedisUtil().setString(valueStrings[0], valueStrings[1]);
-			if (StringUtils.isNoneEmpty(valueStrings[2])) {
-				redisToolDataTableController.getRedisUtil().setDeadLine(valueStrings[0],
-						Integer.parseInt(valueStrings[2]));
-			}
-		}
-	}
+    public RedisToolDataTableService(RedisToolDataTableController redisToolDataTableController) {
+        this.redisToolDataTableController = redisToolDataTableController;
+    }
 
-	private void addList() {
-		RedisToolDialogController redisToolDialogController = RedisToolDialogController.getRedisToolDialogController("添加List数据",true);
-		Optional<ButtonType> _buttonType = redisToolDialogController.getAlert().showAndWait();
-		// 根据点击结果返回
-		System.out.println(_buttonType);
-//		if(_buttonType.get() != null || redisToolDialogController.isEnter()){
-		if(redisToolDialogController.isEnter()){
-			System.out.println(redisToolDialogController.getKeyTextField().getText());
-			String key = redisToolDialogController.getKeyTextField().getText();
-			redisToolDialogController.getDialogTableData().forEach((map)->{
-				map.get("key");
-			});
-		}
-//		if (_buttonType.get().getButtonData().equals(ButtonBar.ButtonData.OK_DONE)) {
-//		}
-	}
+    public void addTableViewContextMenu() {
+        Menu menuAdd = new Menu("添加");
+        MenuItem menuAddString = new MenuItem("String");
+        menuAddString.setOnAction(event1 -> {
+            addString();
+            reloadTableData();
+        });
+        MenuItem menuAddList = new MenuItem("List");
+        menuAddList.setOnAction(event1 -> {
+            redisToolDialogController = RedisToolDialogController.getRedisToolDialogController("List", true);
+            redisToolDialogController.setRedisToolDataTableService(this);
+        });
+        MenuItem menuAddSet = new MenuItem("Set");
+        menuAddSet.setOnAction(event1 -> {
+            redisToolDialogController = RedisToolDialogController.getRedisToolDialogController("Set", true);
+            redisToolDialogController.setRedisToolDataTableService(this);
+        });
+        MenuItem menuAddSortedSet = new MenuItem("Sorted Set");
+        menuAddSortedSet.setOnAction(event1 -> {
+            redisToolDialogController = RedisToolDialogController.getRedisToolDialogController("Sorted Set", false);
+            redisToolDialogController.setRedisToolDataTableService(this);
+        });
+        MenuItem menuAddHash = new MenuItem("Hash");
+        menuAddHash.setOnAction(event1 -> {
+            redisToolDialogController = RedisToolDialogController.getRedisToolDialogController("Hash", false);
+            redisToolDialogController.setRedisToolDataTableService(this);
+        });
+        menuAdd.getItems().addAll(menuAddString, menuAddList, menuAddSet, menuAddSortedSet, menuAddHash);
+        MenuItem menuReload = new MenuItem("刷新");
+        menuReload.setOnAction(event1 -> {
+            reloadTableData();
+        });
+        MenuItem menu_Remove = new MenuItem("删除选中行");
+        menu_Remove.setOnAction(event1 -> {
+            RedisToolDataTableBean redisToolDataTableBean = redisToolDataTableController.getDataTableView()
+                    .getSelectionModel().getSelectedItem();
+            if (redisToolDataTableBean != null) {
+                redisToolDataTableController.getRedisUtil().del(redisToolDataTableBean.getName());
+                redisToolDataTableController.getTableData().remove(redisToolDataTableBean);
+                Tab tab = dataViewTabMap.get(redisToolDataTableBean.getName());
+                if (tab != null) {
+                    redisToolDataTableController.getRedisToolController().getDataViewTabPane().getTabs().remove(tab);
+                    dataViewTabMap.remove(redisToolDataTableBean.getName());
+                }
+            }
+        });
+        MenuItem menu_RemoveAll = new MenuItem("删除所有");
+        menu_RemoveAll.setOnAction(event1 -> {
+            redisToolDataTableController.getRedisUtil().delAll();
+            dataViewTabMap.forEach((String k, Tab tab) -> {
+                redisToolDataTableController.getRedisToolController().getDataViewTabPane().getTabs().remove(tab);
+            });
+            dataViewTabMap.clear();
+            redisToolDataTableController.getTableData().clear();
+        });
+        redisToolDataTableController.getDataTableView()
+                .setContextMenu(new ContextMenu(menuAdd, menuReload, menu_Remove, menu_RemoveAll));
+    }
 
-	private void addSet() {
+    private void addString() {
+        String[] valueStrings = AlertUtil.showInputAlert("请输入值", "key", "value", "过期时间（单位：秒，-1为永久）");
+        if (valueStrings != null) {
+            redisToolDataTableController.getRedisUtil().setString(valueStrings[0], valueStrings[1]);
+            if (StringUtils.isNoneEmpty(valueStrings[2])) {
+                redisToolDataTableController.getRedisUtil().setDeadLine(valueStrings[0],
+                        Integer.parseInt(valueStrings[2]));
+            }
+        }
+    }
 
-	}
+    public void addTableDataByDialog() {
+        switch (redisToolDialogController.getTitle()) {
+            case "List":
+                addList();
+                break;
+            case "Set":
+                addSet();
+                break;
+            case "Sorted Set":
+                addSortedSet();
+                break;
+            case "Hash":
+                addHash();
+                break;
+        }
+        reloadTableData();
+    }
 
-	private void addSortedSet() {
+    private void addList() {
+        String key = redisToolDialogController.getKeyTextField().getText();
+        List<String> list = new ArrayList<String>();
+        redisToolDialogController.getDialogTableData().forEach((map) -> {
+            list.add(map.get("value"));
+        });
+        redisToolDataTableController.getRedisUtil().addList(key,list,false,true);
+        redisToolDataTableController.getRedisUtil().setDeadLine(key,Integer.parseInt(redisToolDialogController.getOverdueTimeTextField().getText()));
+    }
 
-	}
+    private void addSet() {
+        String key = redisToolDialogController.getKeyTextField().getText();
+        Set<String> list = new HashSet<String>();
+        redisToolDialogController.getDialogTableData().forEach((map) -> {
+            list.add(map.get("value"));
+        });
+        redisToolDataTableController.getRedisUtil().addSet(key,list);
+        redisToolDataTableController.getRedisUtil().setDeadLine(key,Integer.parseInt(redisToolDialogController.getOverdueTimeTextField().getText()));
+    }
 
-	private void addHash() {
+    private void addSortedSet() {
+        String key = redisToolDialogController.getKeyTextField().getText();
+        Map<String,Double> list = new HashMap<String,Double>();
+        redisToolDialogController.getDialogTableData().forEach((map) -> {
+            list.put(map.get("key"),Double.valueOf(map.get("value")));
+        });
+        redisToolDataTableController.getRedisUtil().addZSet(key,list);
+        redisToolDataTableController.getRedisUtil().setDeadLine(key,Integer.parseInt(redisToolDialogController.getOverdueTimeTextField().getText()));
+    }
 
-	}
+    private void addHash() {
+        String key = redisToolDialogController.getKeyTextField().getText();
+        Map<String,String> list = new HashMap<String,String>();
+        redisToolDialogController.getDialogTableData().forEach((map) -> {
+            list.put(map.get("key"),map.get("value"));
+        });
+        redisToolDataTableController.getRedisUtil().addHash(key,list);
+        redisToolDataTableController.getRedisUtil().setDeadLine(key,Integer.parseInt(redisToolDialogController.getOverdueTimeTextField().getText()));
+    }
 
-	public void addRedisToolDataViewTab(String key) {
-		Tab tab1 = dataViewTabMap.get(key);
-		if(tab1 != null){
-			redisToolDataTableController.getRedisToolController().getDataViewTabPane().getSelectionModel().select(tab1);
-			return;
-		}
-		final Tab tab = new Tab(key);
-		tab.setOnClosed(new EventHandler<Event>() {
-			@Override
-			public void handle(Event event) {
-				dataViewTabMap.remove(tab.getText());
-			}
-		});
-		FXMLLoader fXMLLoader = RedisToolDataViewController.getFXMLLoader();
-		try {
-			tab.setContent(fXMLLoader.load());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		RedisToolDataViewController redisToolDataViewController = fXMLLoader.getController();
-		redisToolDataViewController.setData(redisToolDataTableController.getRedisUtil(), key);
-		redisToolDataTableController.getRedisToolController().getDataViewTabPane().getTabs().add(tab);
-		redisToolDataTableController.getRedisToolController().getDataViewTabPane().getSelectionModel().select(tab);
-		dataViewTabMap.put(key, tab);
-	}
+    public void addRedisToolDataViewTab(String key) {
+        Tab tab1 = dataViewTabMap.get(key);
+        if (tab1 != null) {
+            redisToolDataTableController.getRedisToolController().getDataViewTabPane().getSelectionModel().select(tab1);
+            return;
+        }
+        final Tab tab = new Tab(key);
+        tab.setOnClosed(new EventHandler<Event>() {
+            @Override
+            public void handle(Event event) {
+                dataViewTabMap.remove(tab.getText());
+            }
+        });
+        FXMLLoader fXMLLoader = RedisToolDataViewController.getFXMLLoader();
+        try {
+            tab.setContent(fXMLLoader.load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        RedisToolDataViewController redisToolDataViewController = fXMLLoader.getController();
+        redisToolDataViewController.setData(redisToolDataTableController.getRedisUtil(), key);
+        redisToolDataTableController.getRedisToolController().getDataViewTabPane().getTabs().add(tab);
+        redisToolDataTableController.getRedisToolController().getDataViewTabPane().getSelectionModel().select(tab);
+        dataViewTabMap.put(key, tab);
+    }
 
-	public void reloadTableData() {
-		redisToolDataTableController.getTableData().clear();
-		RedisUtil redisUtil = redisToolDataTableController.getRedisUtil();
-		Set<String> nodekeys = redisUtil.getKeys();
-		for (String key : nodekeys) {
-			// System.out.println(key);
-			String type = redisUtil.getValueType(key);
-			String size = redisUtil.getSize(key).toString();
-			Long timeLong = redisUtil.getDeadline(key);
-			String time = timeLong == -1 ? "永久" : timeLong.toString();
-			RedisToolDataTableBean redisToolDataTableBean = new RedisToolDataTableBean(key, type, size, time);
-			redisToolDataTableController.getTableData().add(redisToolDataTableBean);
-		}
-	}
+    public void reloadTableData() {
+        redisToolDataTableController.getTableData().clear();
+        RedisUtil redisUtil = redisToolDataTableController.getRedisUtil();
+        Set<String> nodekeys = redisUtil.getKeys();
+        for (String key : nodekeys) {
+            // System.out.println(key);
+            String type = redisUtil.getValueType(key);
+            String size = redisUtil.getSize(key).toString();
+            Long timeLong = redisUtil.getDeadline(key);
+            String time = timeLong == -1 ? "永久" : timeLong.toString();
+            RedisToolDataTableBean redisToolDataTableBean = new RedisToolDataTableBean(key, type, size, time);
+            redisToolDataTableController.getTableData().add(redisToolDataTableBean);
+        }
+    }
 
 }
