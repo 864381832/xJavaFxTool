295739530da240fb46f3525c69877d752258424d
XUFENG-PC\xufeng <1277032935@qq.com>
2019-04-09 18:15:00 +0800

    添加zookeeper工具。

--- src/main/java/com/xwintop/xJavaFxTool/controller/debugTools/ZookeeperToolController.java
+++ src/main/java/com/xwintop/xJavaFxTool/controller/debugTools/ZookeeperToolController.java
@@ -1,15 +1,21 @@
 package com.xwintop.xJavaFxTool.controller.debugTools;
 
+import com.easipass.gateway.entity.TaskConfig;
 import com.xwintop.xJavaFxTool.services.debugTools.ZookeeperToolService;
 import com.xwintop.xJavaFxTool.view.debugTools.ZookeeperToolView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TreeItem;
+import javafx.scene.input.MouseButton;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.DateFormatUtils;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 /**
@@ -38,6 +44,55 @@ public class ZookeeperToolController extends ZookeeperToolView {
     }
 
     private void initEvent() {
+        nodeTreeView.setOnMouseClicked(event -> {
+            TreeItem<String> selectedItem = nodeTreeView.getSelectionModel().getSelectedItem();
+            if (selectedItem == null) {
+                return;
+            }
+            if (event.getButton() == MouseButton.PRIMARY) {
+                zookeeperToolService.nodeSelectionChanged(selectedItem);
+            } else if (event.getButton() == MouseButton.SECONDARY) {
+                MenuItem menu_UnfoldAll = new MenuItem("展开所有");
+                menu_UnfoldAll.setOnAction(event1 -> {
+                    nodeTreeView.getRoot().setExpanded(true);
+                    nodeTreeView.getRoot().getChildren().forEach(stringTreeItem -> {
+                        stringTreeItem.setExpanded(true);
+                    });
+                });
+                MenuItem menu_FoldAll = new MenuItem("折叠所有");
+                menu_FoldAll.setOnAction(event1 -> {
+                    nodeTreeView.getRoot().setExpanded(false);
+                    nodeTreeView.getRoot().getChildren().forEach(stringTreeItem -> {
+                        stringTreeItem.setExpanded(false);
+                    });
+                });
+                ContextMenu contextMenu = new ContextMenu(menu_UnfoldAll, menu_FoldAll);
+                if (selectedItem.getValue().endsWith("service.yml")) {
+                    MenuItem menu_AddTask = new MenuItem("添加任务");
+                    menu_AddTask.setOnAction(event1 -> {
+                        String taskConfigName = "taskConfig" + DateFormatUtils.format(new Date(), "MMddHHmm");
+                        TreeItem<String> addItem = new TreeItem<>(taskConfigName);
+                        selectedItem.getChildren().add(addItem);
+                        TaskConfig taskConfig = new TaskConfig();
+                        taskConfig.setName(taskConfigName);
+                    });
+                    contextMenu.getItems().add(menu_AddTask);
+                    MenuItem menu_RemoveFile = new MenuItem("删除文件");
+                    menu_RemoveFile.setOnAction(event1 -> {
+                        selectedItem.getParent().getChildren().remove(selectedItem);
+                    });
+                    contextMenu.getItems().add(menu_RemoveFile);
+                    MenuItem menu_RemoveAll = new MenuItem("删除所有任务");
+                    menu_RemoveAll.setOnAction(event1 -> {
+                        selectedItem.getChildren().clear();
+                    });
+                    contextMenu.getItems().add(menu_RemoveAll);
+                    MenuItem menu_SaveFile = new MenuItem("保存文件");
+                    contextMenu.getItems().add(menu_SaveFile);
+                }
+                nodeTreeView.setContextMenu(contextMenu);
+            }
+        });
     }
 
     private void initService() {
@@ -60,7 +115,7 @@ public class ZookeeperToolController extends ZookeeperToolView {
 
     @FXML
     private void deleteNodeOnAction(ActionEvent event) {
-        zookeeperToolService.disconnectOnAction();
+        zookeeperToolService.deleteNodeOnAction();
     }
 
     @FXML
@@ -70,7 +125,7 @@ public class ZookeeperToolController extends ZookeeperToolView {
 
     @FXML
     private void nodeDataSaveOnAction(ActionEvent event) {
-
+        zookeeperToolService.nodeDataSaveOnAction();
     }
 
 
--- src/main/java/com/xwintop/xJavaFxTool/services/debugTools/ZookeeperToolService.java
+++ src/main/java/com/xwintop/xJavaFxTool/services/debugTools/ZookeeperToolService.java
@@ -1,14 +1,25 @@
 package com.xwintop.xJavaFxTool.services.debugTools;
 
 import com.xwintop.xJavaFxTool.controller.debugTools.ZookeeperToolController;
+import com.xwintop.xcore.util.javafx.AlertUtil;
+import com.xwintop.xcore.util.javafx.TooltipUtil;
 import javafx.scene.control.TreeItem;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.exception.ZkMarshallingError;
+import org.I0Itec.zkclient.serialize.ZkSerializer;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.zookeeper.ZooDefs.Perms;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Stat;
 
+import java.io.UnsupportedEncodingException;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName: ZookeeperToolService
@@ -32,10 +43,29 @@ public class ZookeeperToolService {
     public void connectOnAction() {
         if (zkClient == null) {
             zkClient = new ZkClient(zookeeperToolController.getZkServersTextField().getText().trim());
-//            zkClient.setZkSerializer(new ZKStringSerializer());
+            zkClient.setZkSerializer(new ZkSerializer() {
+                @Override
+                public byte[] serialize(Object data) throws ZkMarshallingError {
+                    try {
+                        return String.valueOf(data).getBytes("utf-8");
+                    } catch (UnsupportedEncodingException var3) {
+                        throw new ZkMarshallingError(var3.getMessage());
+                    }
+                }
+
+                @Override
+                public Object deserialize(byte[] bytes) throws ZkMarshallingError {
+                    try {
+                        return new String(bytes, "utf-8");
+                    } catch (UnsupportedEncodingException e) {
+                        throw new ZkMarshallingError(e.getMessage());
+                    }
+                }
+            });
         }
         zookeeperToolController.getNodeTreeView().getRoot().getChildren().clear();
         this.addNodeTree("/", zookeeperToolController.getNodeTreeView().getRoot());
+        zookeeperToolController.getNodeTreeView().getRoot().setExpanded(true);
     }
 
     private void addNodeTree(String path, TreeItem<String> treeItem) {
@@ -48,23 +78,111 @@ public class ZookeeperToolService {
         }
     }
 
-    public void disconnectOnAction() {
+    private String getNodePath(TreeItem<String> selectedItem) {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(selectedItem.getValue());
+        TreeItem<String> indexItem = selectedItem.getParent();
+        while (indexItem != null) {
+            stringBuffer.insert(0, StringUtils.appendIfMissing(indexItem.getValue(), "/", "/"));
+            indexItem = indexItem.getParent();
+        }
+        return stringBuffer.toString();
+    }
 
+    public void nodeSelectionChanged(TreeItem<String> selectedItem) {
+        String nodePath = this.getNodePath(selectedItem);
+        if (!zkClient.exists(nodePath)) {
+            TooltipUtil.showToast("结点不存在");
+            return;
+        }
+        zookeeperToolController.getNodeDataValueTextArea().setText(zkClient.readData(nodePath));
+        Map.Entry<List<ACL>, Stat> aclsEntry = zkClient.getAcl(nodePath);
+        Stat stat = aclsEntry.getValue();
+        zookeeperToolController.getA_VERSIONTextField().setText("" + stat.getAversion());
+        zookeeperToolController.getC_TIMETextField().setText(DateFormatUtils.format(stat.getCtime(), "yyyy-MM-dd'T'HH:mm:ss.SSS z"));
+        zookeeperToolController.getC_VERSIONTextField().setText("" + stat.getCversion());
+        zookeeperToolController.getCZXIDTextField().setText("0x" + Long.toHexString(stat.getCzxid()));
+        zookeeperToolController.getDATA_LENGTHTextField().setText("" + stat.getDataLength());
+        zookeeperToolController.getEPHEMERAL_OWNERTextField().setText("0x" + Long.toHexString(stat.getEphemeralOwner()));
+        zookeeperToolController.getM_TIMETextField().setText(DateFormatUtils.format(stat.getMtime(), "yyyy-MM-dd'T'HH:mm:ss.SSS z"));
+        zookeeperToolController.getMZXIDTextField().setText("0x" + Long.toHexString(stat.getMzxid()));
+        zookeeperToolController.getNUM_CHILDRENTextField().setText("" + stat.getNumChildren());
+        zookeeperToolController.getPZXIDTextField().setText("0x" + Long.toHexString(stat.getPzxid()));
+        zookeeperToolController.getVERSIONTextField().setText("" + stat.getVersion());
+
+        List<ACL> acls = aclsEntry.getKey();
+        for (ACL acl : acls) {
+            Map<String, String> aclMap = new LinkedHashMap<String, String>();
+            zookeeperToolController.getAclSchemeTextField().setText(acl.getId().getScheme());
+            zookeeperToolController.getAclIdTextField().setText(acl.getId().getId());
+            StringBuilder sb = new StringBuilder();
+            int perms = acl.getPerms();
+            if ((perms & Perms.READ) == Perms.READ) {
+                sb.append("Read");
+            }
+            if ((perms & Perms.WRITE) == Perms.WRITE) {
+                sb.append(", Write");
+            }
+            if ((perms & Perms.CREATE) == Perms.CREATE) {
+                sb.append(", Create");
+            }
+            if ((perms & Perms.DELETE) == Perms.DELETE) {
+                sb.append(", Delete");
+            }
+            if ((perms & Perms.ADMIN) == Perms.ADMIN) {
+                sb.append(", Admin");
+            }
+            zookeeperToolController.getAclPermissionsTextField().setText(sb.toString());
+        }
+    }
+
+    public void disconnectOnAction() {
+        if(zkClient != null){
+            zkClient.close();
+            zkClient = null;
+        }
+        zookeeperToolController.getNodeTreeView().getRoot().getChildren().clear();
     }
 
     public void refreshOnAction() {
-
+        if(zkClient == null){
+            TooltipUtil.showToast("zookeeper未连接");
+            return;
+        }
+        connectOnAction();
     }
 
     public void deleteNodeOnAction() {
-
+        TreeItem<String> selectedItem = zookeeperToolController.getNodeTreeView().getSelectionModel().getSelectedItem();
+        if (selectedItem == null) {
+            TooltipUtil.showToast("未选中结点");
+            return;
+        }
+        String nodePath = this.getNodePath(selectedItem);
+        zkClient.delete(nodePath);
+        selectedItem.getParent().getChildren().remove(selectedItem);
     }
 
     public void addNodeOnAction() {
-
+        TreeItem<String> selectedItem = zookeeperToolController.getNodeTreeView().getSelectionModel().getSelectedItem();
+        if (selectedItem == null) {
+            TooltipUtil.showToast("未选中结点");
+            return;
+        }
+        String nodeName = AlertUtil.showInputAlert("请输入结点名称：");
+        String nodePath = this.getNodePath(selectedItem);
+        zkClient.createEphemeral(StringUtils.appendIfMissing(nodePath, "/", "/") + nodeName);
+        TreeItem<String> treeItem2 = new TreeItem<>(nodeName);
+        selectedItem.getChildren().add(treeItem2);
     }
 
     public void nodeDataSaveOnAction() {
-
+        TreeItem<String> selectedItem = zookeeperToolController.getNodeTreeView().getSelectionModel().getSelectedItem();
+        if (selectedItem == null) {
+            TooltipUtil.showToast("未选中结点");
+            return;
+        }
+        String nodePath = this.getNodePath(selectedItem);
+        zkClient.writeData(nodePath, zookeeperToolController.getNodeDataValueTextArea().getText());
     }
 }
