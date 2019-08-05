64cf288d570488f8f78967ffb7d529a018d55cfe
xufeng <1277032935@qq.com>
2019-08-05 23:59:24 +0800

    1、优化文件搜索线程处理

--- src/main/java/com/xwintop/xJavaFxTool/services/littleTools/FileSearchToolService.java
+++ src/main/java/com/xwintop/xJavaFxTool/services/littleTools/FileSearchToolService.java
@@ -1,6 +1,5 @@
 package com.xwintop.xJavaFxTool.services.littleTools;
 
-import cn.hutool.core.thread.ThreadUtil;
 import com.xwintop.xJavaFxTool.controller.littleTools.FileSearchToolController;
 import com.xwintop.xJavaFxTool.utils.ConfigureUtil;
 import com.xwintop.xcore.util.javafx.TooltipUtil;
@@ -16,6 +15,7 @@ import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.NoLockFactory;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,6 +24,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @ClassName: FileSearchToolService
@@ -38,6 +39,7 @@ import java.util.*;
 public class FileSearchToolService {
     private FileSearchToolController fileSearchToolController;
     private static Timer autoRefreshIndexTimer = null;
+    private static ThreadPoolTaskExecutor refreshIndexPoolTaskExecutor;
     private static final String searchIndexDir = ConfigureUtil.getConfigurePath("searchIndexDir/");
 
     private static Directory directory;
@@ -61,6 +63,13 @@ public class FileSearchToolService {
         } catch (Exception e) {
             log.error("创建文件失败！", e);
         }
+        refreshIndexPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        refreshIndexPoolTaskExecutor.setCorePoolSize(5);
+        refreshIndexPoolTaskExecutor.setMaxPoolSize(20);
+        refreshIndexPoolTaskExecutor.setQueueCapacity(30);
+        // 设置拒绝策略
+        refreshIndexPoolTaskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        refreshIndexPoolTaskExecutor.initialize();
     }
 
     public static void getIndexWriter(Directory directory) throws IOException {
@@ -185,7 +194,7 @@ public class FileSearchToolService {
     }
 
     public void addSearchIndexFile(Path path) {
-        ThreadUtil.execute(() -> {
+        refreshIndexPoolTaskExecutor.execute(() -> {
             try {
                 DirectoryStream<Path> stream = Files.newDirectoryStream(path);
                 Iterator<Path> pathIterator = stream.iterator();
