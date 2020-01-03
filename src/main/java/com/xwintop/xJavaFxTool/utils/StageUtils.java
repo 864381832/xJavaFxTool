e940e302baa6c4f8f96ce905560a443483283910
xufeng <1277032935@qq.com>
2020-01-03 23:14:15 +0800

    修复窗口最小化mac平台兼容问题

--- src/main/java/com/xwintop/xJavaFxTool/utils/StageUtils.java
+++ src/main/java/com/xwintop/xJavaFxTool/utils/StageUtils.java
@@ -3,6 +3,7 @@ package com.xwintop.xJavaFxTool.utils;
 import com.sun.javafx.tk.TKStage;
 import com.sun.jna.LastErrorException;
 import com.sun.jna.Native;
+import com.sun.jna.Platform;
 import com.sun.jna.Pointer;
 import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinDef;
@@ -12,6 +13,7 @@ import com.sun.jna.win32.W32APIOptions;
 import javafx.stage.Stage;
 
 import java.lang.reflect.Method;
+
 /**
  * @ClassName: StageUtils
  * @Description: 更新场景工具类（解决点击任务栏图标无法最小化问题）
@@ -23,19 +25,23 @@ public class StageUtils {
 
     static interface ExtUser32 extends StdCallLibrary, User32 {
         ExtUser32 INSTANCE = (ExtUser32) Native.loadLibrary("user32", ExtUser32.class, W32APIOptions.DEFAULT_OPTIONS);
+
         WinDef.LRESULT CallWindowProcW(Pointer lpWndProc, Pointer hWnd, int msg, WinDef.WPARAM wParam, WinDef.LPARAM lParam);
+
         int SetWindowLong(HWND hWnd, int nIndex, com.sun.jna.Callback wndProc) throws LastErrorException;
     }
 
     // update default javafx stage style
     public static void updateStageStyle(Stage stage) {
-        Pointer pointer = getWindowPointer(stage);
-        WinDef.HWND hwnd = new WinDef.HWND(pointer);
+        if (Platform.isWindows()) {
+            Pointer pointer = getWindowPointer(stage);
+            WinDef.HWND hwnd = new WinDef.HWND(pointer);
 
-        final User32 user32 = User32.INSTANCE;
-        int oldStyle = user32.GetWindowLong(hwnd, WinUser.GWL_STYLE);
-        int newStyle = oldStyle | 0x00020000; // WS_MINIMIZEBOX
-        user32.SetWindowLong(hwnd, WinUser.GWL_STYLE, newStyle);
+            final User32 user32 = User32.INSTANCE;
+            int oldStyle = user32.GetWindowLong(hwnd, WinUser.GWL_STYLE);
+            int newStyle = oldStyle | 0x00020000; // WS_MINIMIZEBOX
+            user32.SetWindowLong(hwnd, WinUser.GWL_STYLE, newStyle);
+        }
     }
 
     private static Pointer getWindowPointer(Stage stage) {
