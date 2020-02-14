9a1c431305c5e38c0e3c5608fa79a228e4c97721
He Yiding <yiding.he@gmail.com>
2020-02-14 17:52:37 +0800

    将记事本单独提取出来，以便将来进一步开发

---
+++ src/main/java/com/xwintop/xJavaFxTool/controller/NotepadController.java
@@ -0,0 +1,9 @@
+package com.xwintop.xJavaFxTool.controller;
+
+import com.xwintop.xJavaFxTool.view.NotepadView;
+
+public class NotepadController extends NotepadView {
+
+    public void initialize() {
+    }
+}
---
+++ src/main/java/com/xwintop/xJavaFxTool/view/NotepadView.java
@@ -0,0 +1,10 @@
+package com.xwintop.xJavaFxTool.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public abstract class NotepadView {
+
+    @FXML
+    protected TextArea textArea;
+}
