c86ce3238dd7b23e0610ce27dc557846740d3dca
He Yiding <yiding.he@gmail.com>
2020-02-24 16:21:38 +0800

    记住上次退出时的主窗体位置

--- src/main/java/com/xwintop/xJavaFxTool/utils/Config.java
+++ src/main/java/com/xwintop/xJavaFxTool/utils/Config.java
@@ -6,8 +6,30 @@ import java.util.Locale;
  * 配置文件
  */
 public class Config {
-	public static Locale defaultLocale = Locale.getDefault();// 设置系统语言
 
-	public static final String xJavaFxToolVersions = "V0.2.0";// xJavaFxTool版本信息
-	public static final int xJavaFxToolVersionsInteger = 12;// xJavaFxTool更新信息
+    public static Locale defaultLocale = Locale.getDefault();// 设置系统语言
+
+    public static final String xJavaFxToolVersions = "V0.2.0";// xJavaFxTool版本信息
+
+    public static final int xJavaFxToolVersionsInteger = 12;// xJavaFxTool更新信息
+
+	/**
+	 * 集中存放配置 key 的地方
+	 */
+    public static class Keys {
+
+		/**
+		 * 窗体相关配置
+		 */
+		public static class MainWindow {
+
+            public static final String WIDTH = "main-window.width";
+
+            public static final String HEIGHT = "main-window.height";
+
+            public static final String TOP = "main-window.top";
+
+            public static final String LEFT = "main-window.left";
+        }
+    }
 }
