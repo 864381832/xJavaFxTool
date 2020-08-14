a35e884c070a211e21f33f5a693fb74c3a0eecfb
xufeng <1277032935@qq.com>
2020-08-14 23:42:09 +0800

    优化类加载器

--- src/main/java/com/xwintop/xJavaFxTool/utils/XJavaFxSystemUtil.java
+++ src/main/java/com/xwintop/xJavaFxTool/utils/XJavaFxSystemUtil.java
@@ -4,6 +4,7 @@ import com.xwintop.xJavaFxTool.Main;
 import com.xwintop.xJavaFxTool.plugin.PluginManager;
 import com.xwintop.xJavaFxTool.services.index.PluginManageService;
 import java.io.File;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -51,7 +52,7 @@ public class XJavaFxSystemUtil {
             File libPath = new File("libs/");
             // 获取所有的.jar和.zip文件
             File[] jarFiles = libPath.listFiles(
-                    (dir, name) -> name.endsWith(".jar")
+                (dir, name) -> name.endsWith(".jar")
             );
             if (jarFiles != null) {
                 for (File file : jarFiles) {
@@ -72,16 +73,39 @@ public class XJavaFxSystemUtil {
      * @Description: 添加jar包到系统中
      */
     public static void addJarClass(File jarFile) {
-//        try {
-//            log.info("Reading lib file: " + jarFile.getName());
-//            Method method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
-//            method.setAccessible(true); // 设置方法的访问权限
-//            // 获取系统类加载器
-//            URLClassLoader classLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-//            URL url = jarFile.toURI().toURL();
-//            method.invoke(classLoader, url);
-//        } catch (Exception e) {
-//            log.error("添加libs中jar包到系统中异常:", e);
-//        }
+        try {
+            ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+            URL url = jarFile.toURI().toURL();
+            if (classLoader instanceof URLClassLoader) {
+                System.out.println("DEB: classLoader instanceof URLClassLoader");
+                URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+                Class sysclass = URLClassLoader.class;
+                try {
+                    Method method = sysclass.getDeclaredMethod("addURL", URL.class);
+                    method.setAccessible(true);
+                    method.invoke(sysloader, url);
+                } catch (Exception var5) {
+                    var5.printStackTrace();
+                    throw new IllegalStateException(var5.getMessage(), var5);
+                }
+            } else {
+                try {
+                    Field field = classLoader.getClass().getDeclaredField("ucp");
+                    field.setAccessible(true);
+                    Object ucp = field.get(classLoader);
+
+                    System.out.println("DEB: invoke method!");
+                    Method method = ucp.getClass().getDeclaredMethod("addURL", URL.class);
+                    method.setAccessible(true);
+
+                    method.invoke(ucp, url);
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                    throw new IllegalStateException(exception.getMessage(), exception);
+                }
+            }
+        } catch (Exception e) {
+            log.error("添加libs中jar包到系统中异常:", e);
+        }
     }
 }
