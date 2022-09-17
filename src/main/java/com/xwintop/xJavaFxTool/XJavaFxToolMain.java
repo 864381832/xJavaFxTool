10fd1cdc9613b624509dc7c0bea2cb6078ef033a
xufeng <1277032935@qq.com>
2022-09-18 00:10:48 +0800

    修改启动类方法

--- src/main/java/com/xwintop/xJavaFxTool/XJavaFxToolMain.java
+++ src/main/java/com/xwintop/xJavaFxTool/XJavaFxToolMain.java
@@ -1,15 +1,14 @@
 /**
- *    Copyright (c) [2019] [xufeng]
- *    [xJavaFxTool] is licensed under Mulan PSL v2.
- *    You can use this software according to the terms and conditions of the Mulan PSL v2.
- *    You may obtain a copy of Mulan PSL v2 at:
- *             http://license.coscl.org.cn/MulanPSL2
- *    THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
- *    See the Mulan PSL v2 for more details.
+ * Copyright (c) [2019] [xufeng]
+ * [xJavaFxTool] is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ * http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
  */
 package com.xwintop.xJavaFxTool;
 
-import com.xwintop.xJavaFxTool.utils.XJavaFxSystemUtil;
 import javafx.application.Application;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,9 +17,7 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class XJavaFxToolMain {
-
     public static void main(String[] args) {
-        XJavaFxSystemUtil.initSystemLocal();    // 初始化本地语言
         Application.launch(XJavaFxToolApplication.class, args);
     }
 }
