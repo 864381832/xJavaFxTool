08bfc8f41f526e261c36cf2add27fe0c96bddd91
xufeng <1277032935@qq.com>
2024-01-20 17:54:46 +0800

    1.升级相关依赖包版本

--- xJavaFxTool/src/main/java/com/xwintop/xJavaFxTool/utils/VersionChecker.java
+++ xJavaFxTool/src/main/java/com/xwintop/xJavaFxTool/utils/VersionChecker.java
@@ -1,8 +1,8 @@
 package com.xwintop.xJavaFxTool.utils;
 
 import cn.hutool.http.HttpUtil;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.xwintop.xcore.javafx.dialog.FxAlerts;
 import com.xwintop.xcore.util.HttpClientUtil;
 import lombok.extern.slf4j.Slf4j;
