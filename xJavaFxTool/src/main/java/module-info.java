1dec00770cf486c15216fcde15846e42264fc0cf
xufeng <1277032935@qq.com>
2024-01-21 17:12:33 +0800

    1.修改工具类依赖

--- xJavaFxTool/src/main/java/module-info.java
+++ xJavaFxTool/src/main/java/module-info.java
@@ -7,14 +7,9 @@ module com.xwintop.xJavaFxTool {
     opens com.xwintop.xJavaFxTool.controller.plugin;
 
     requires com.xwintop.xcore;
-    requires static lombok;
-    requires org.slf4j;
-    requires org.apache.commons.lang3;
-    requires javafx.web;
     requires java.sql;
+    requires java.xml;
     requires io.github.classgraph;
-    requires okhttp3;
-    requires org.apache.commons.io;
 //    requires okio;
     requires commons.configuration;
     requires ch.qos.logback.classic;
